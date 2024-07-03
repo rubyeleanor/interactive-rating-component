82b71193f7899a0ff8e949f421568a05fc651e0b
rubyhay <115424833+rubyeleanor@users.noreply.github.com>
2024-07-03 18:40:19 +0100

    JS Update and error msg
    JS script completed and error message added if no rating selected

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const ratings = document.querySelectorAll('.nums');
 const submit = document.querySelector('#submit');
 const score = document.querySelector('#score');
+const selection = document.querySelector('#scoreSelect');
+const thanks = document.querySelector('#thankYouMsg');
+const errorMsg = document.querySelector('#errorMsg');
 
+let selectedScore = null;
 
 
 for (let i = 0; i < ratings.length; i++) {
     ratings[i].addEventListener('click', () => {
-        score.textContent = i + 1;
+        selectedScore = i + 1;
+        score.textContent = selectedScore;
+    });
+};
 
-    })
-}
+submit.addEventListener('click', () => {
+    if (selectedScore) {
+        selection.classList.add('hidden');
+        thanks.classList.remove('hidden');
+    } else {
+        errorMsg.classList.remove('hidden')
+    }
+
+});
