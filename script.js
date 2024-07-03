e5c2be724dfc320ad0c5c0e1a3ba99e5ae746495
rubyhay <115424833+rubyeleanor@users.noreply.github.com>
2024-07-03 13:41:54 +0100

    Score change added
    Basic JS to change text on thank you content added

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-const buttons = document.querySelectorAll('.nums');
+const ratings = document.querySelectorAll('.nums');
+const submit = document.querySelector('#submit');
+const score = document.querySelector('#score');
 
+
+
+for (let i = 0; i < ratings.length; i++) {
+    ratings[i].addEventListener('click', () => {
+        score.textContent = i + 1;
+
+    })
+}
